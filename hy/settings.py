f5d0134b3a61cd98aabff45ddf3eda7966c676dd
Riyas N S <riyasns871@gmail.com>
2025-10-04 23:30:37 +0530

    Remove secrets and use environment variables

--- hy/settings.py
+++ hy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-#9mo$rgq(flq!2wt=9+vms0=ejt=_m8w_wi3hg$_-m0++he5hw'
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
