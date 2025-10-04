e831b93966c3e1c5e4f6483ce47e691bcec261b3
Riyas N S <riyasns871@gmail.com>
2025-10-05 00:19:50 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- hy/settings.py
+++ hy/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+import os
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+
 
 # Application definition
 
