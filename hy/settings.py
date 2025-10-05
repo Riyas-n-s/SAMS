9fa83caf91bceef58bd17834f5b1cedbbaec6d4b
Riyas N S <riyasns871@gmail.com>
2025-10-05 21:13:16 +0530

    Fix static files path

--- hy/settings.py
+++ hy/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "accounts" / "static", ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files
